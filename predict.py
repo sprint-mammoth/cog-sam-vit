4779b1dab23b5762121625dd9b6e9e59bbd2e5bf
peter65374 <1454994@qq.com>
2023-10-17 12:56:38 +0800

    改成返回 float list embedding value 像 replicate 的 embedding 例子一样

--- predict.py
+++ predict.py
@@ -1,13 +1,17 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path, File
-import torch
+from cog import BasePredictor, BaseModel, Input, Path
 import numpy as np
 import cv2
-import io
-import tempfile
+# import io
+# import tempfile
 from segment_anything import sam_model_registry, SamPredictor
+
+
+class Embedding(BaseModel):
+    shape: tuple[int, int, int, int]
+    embedding: list[float]
 
 
 class Predictor(BasePredictor):
@@ -20,10 +24,20 @@ class Predictor(BasePredictor):
         sam.to(device='cuda')
         self.predictor = SamPredictor(sam)
 
+    def reshape_embedding(self, embedding_list: list[float], shape: tuple[int, int, int, int]=(1, 256, 64, 64)) -> np.ndarray:
+        """Reshape the embedding_list to the orignal shape"""
+        # Convert list to numpy array
+        flattened_array = np.array(embedding_list)
+
+        # Reshape the flattened array to its original shape
+        original_shape_array = flattened_array.reshape(shape)
+
+        return original_shape_array
+    
     def predict(
         self,
         source_image: Path = Input(description="input image file handler"),
-    ) -> Path:
+    ) -> Embedding:
         """Run a single prediction on the model"""
         try:
             # processed_input = preprocess(image)
@@ -39,11 +53,16 @@ class Predictor(BasePredictor):
             # output = self.model(processed_image)
             self.predictor.set_image(image_rgb)
             image_embedding = self.predictor.get_image_embedding().cpu().numpy()
-
-            print(image_embedding.shape)
+            
+            shape = image_embedding.shape
+            print(shape) # shape supposed to be (1, 256, 64, 64)
             print("successfully make image embedding\n")
 
             # return postprocess(output)
+            # flatten the image embedding and return it as a list of float
+            output = Embedding(shape=shape, embedding=[float(x) for x in np.array(image_embedding).flatten().tolist()])
+            return output
+
             ''' # At the start of your predict function, use a NamedTemporaryFile:
             with tempfile.NamedTemporaryFile(suffix=".npy", delete=False) as temp_file:
                 if temp_file is None:
@@ -55,13 +74,13 @@ class Predictor(BasePredictor):
                 # Return the file handle
                 return File(temp_file)'''
         
-            # Save the image embedding to a temporary numpy array file
+            '''# Save the image embedding to a temporary numpy array file
             # This file will automatically be deleted by Cog after it has been returned.
             with tempfile.NamedTemporaryFile(suffix=".npy", delete=False) as temp_file:
                 np.save(temp_file.name, image_embedding)
                 temp_path = temp_file.name
                 print(f"embedding file is saved to temp_path: {temp_path}\n")
 
-            return Path(temp_path)
+            return Path(temp_path)'''
         except Exception as e:
             raise ValueError(f"Error processing image: {e}")
